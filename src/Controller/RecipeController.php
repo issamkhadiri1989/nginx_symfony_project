5695934262c2fca53bb3e8cc43d770e74f1d48e1
Issam KHADIRI <khadiri.issam@gmail.com>
2024-09-17 16:45:42 +0100

    include parial templates using controller Twig function

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class RecipeController extends AbstractController
+{
+    #[Route('/recipe', name: 'app_recipe')]
+    public function index(): Response
+    {
+        return $this->render('recipe/index.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+
+
+    public function lastRecipes(int $limit = 10): Response
+    {
+        // traitement par exemple communiquer avec la base de données ou API
+
+        return $this->render('recipe/_last_recipes.html.twig', [
+            'controller_name' => 'RecipeController',
+        ]);
+    }
+}
