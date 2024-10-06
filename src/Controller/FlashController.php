172173d8ac23c93461e9ae209aafed2d987fd857
Issam KHADIRI <khadiri.issam@gmail.com>
2024-10-06 12:23:32 +0100

    add new notification center

---
+++ src/Controller/FlashController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class FlashController extends AbstractController
+{
+    #[Route('/flash', name: 'app_flash')]
+    public function index(): Response
+    {
+        $this->addFlash('success', 'Well done ! you made it');
+        $this->addFlash('warning', 'Beware ! Something is outdated');
+        $this->addFlash('danger', 'Error ! The system is under fire.');
+
+        return $this->render('flash/index.html.twig', [
+            'controller_name' => 'FlashController',
+        ]);
+    }
+}
