54c79f2963650ca97ce1f463d9732b06aaf8bff2
Issam KHADIRI <khadiri.issam@gmail.com>
2024-10-19 19:18:14 +0100

    forms: add the Blog class

---
+++ src/DTO/Blog.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DTO;
+
+class Blog
+{
+    private string $title;
+    private string $body;
+    private \DateTimeInterface $publishedAt;
+    private bool $published;
+
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    public function getBody(): string
+    {
+        return $this->body;
+    }
+
+    public function setBody(string $body): void
+    {
+        $this->body = $body;
+    }
+
+    public function getPublishedAt(): \DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $publishedAt): void
+    {
+        $this->publishedAt = $publishedAt;
+    }
+
+    public function isPublished(): bool
+    {
+        return $this->published;
+    }
+
+    public function setPublished(bool $published): void
+    {
+        $this->published = $published;
+    }
+}
