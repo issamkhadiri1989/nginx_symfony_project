b3e4c1be7a35fea4dc99d830441f50af18c59895
Issam KHADIRI <khadiri.issam@gmail.com>
2024-10-20 11:42:08 +0100

    forms: use twig functions to customize forms

--- src/Form/Type/BlogType.php
+++ src/Form/Type/BlogType.php
@@ -6,6 +6,7 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 
@@ -14,6 +15,7 @@ class BlogType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('title', TextType::class)
+            ->add('body', TextareaType::class)
                 ->add('submit', SubmitType::class);
     }
 }
