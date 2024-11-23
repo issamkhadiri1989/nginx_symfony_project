f6de9fbab8eaa91de7ef26c34e4152d9f9622ce7
Issam KHADIRI <khadiri.issam@gmail.com>
2024-11-23 19:59:06 +0100

    ApiPlatform

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
